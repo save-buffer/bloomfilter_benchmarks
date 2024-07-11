01b503860d4d7652c0546324e000daaa3761cb9c
Sasha Krassovsky <krassovskysasha@gmail.com>
2024-07-10 21:17:39 -0700

    Turn on compiler warnings

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-g++ bench.cpp -o bench -O3 -ggdb -mavx2
-g++ fpr.cpp -o fpr -O3 -ggdb -mavx2
+g++ bench.cpp -o bench -O3 -ggdb -mavx2 -Wall -Werror
+g++ fpr.cpp -o fpr -O3 -ggdb -mavx2 -Wall -Werror
 #g++ fpr.cpp -o fpr -ggdb -mavx2 -fsanitize=undefined -fsanitize=address
 
